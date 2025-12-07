c6c0add33a481c5e9cadc89302432ec38bf2349e
nvbao117 <baocute056@gmail.com>
2025-12-07 15:53:26 +0700

    add entity cua bao

---
+++ src/main/java/com/example/online_shoe_store/Entity/Brand.java
@@ -0,0 +1,24 @@
+package com.example.online_shoe_store.Entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "brands")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Brand {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "brand_id")
+    private Long brandId;
+
+    @Column(name = "name", nullable = false, unique = true, length = 100)
+    private String name;
+
+    @Column(name = "description", columnDefinition = "TEXT")
+    private String description;
+}
